9a349090cb0cec538c73378d9225f5533ddd2fdb
Kirill Yatsenko <kirillyat@gmail.com>
2024-12-29 20:20:49 +0300

    init kirillyat.ru

---
+++ frontend/src/api.js
@@ -0,0 +1,14 @@
+
+
+const apiConfig = {
+  baseUrl: 'http://localhost:8000',
+  endpoints: {
+    message: '/message/',
+  },
+  headers: {
+    'Content-Type': 'application/json',
+  }
+};
+
+export default apiConfig;
+
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+
+import '../styles/Navbar.css';
+
+
+function KiriLLM() {
+  return (
+    <div className="KiriLLM">
+      kiriLLM
+    </div>
+  );
+}
+
+ export default function Navbar() {
+    return (
+      <nav className="Navbar">
+        <KiriLLM />
+      </nav>
+    );
+  }
+  
+
